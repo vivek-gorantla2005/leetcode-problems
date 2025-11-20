ac89f053ef92ec2e68c19d345ca09b09ea967168
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-11-20 06:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.56%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int es = 0;
+        int os = 0;
+        for(int i = 0 ; i < num.size();i++)   {
+            if(i%2 == 0){
+                es+=num[i]-'0';
+            }else{
+                os += num[i]-'0';
+            }
+        }
+
+        return (es == os)?true:false;
+    }
+};

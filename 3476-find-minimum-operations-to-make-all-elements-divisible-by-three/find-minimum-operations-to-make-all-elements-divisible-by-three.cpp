4bf3aac244c40a44af4bed6543f7bc278f40b62a
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-11-22 06:01:53 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (45.48%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,9 +3,7 @@ public:
     int minimumOperations(vector<int>& nums) {
         int cnt = 0;
         for(int i : nums){
-            if((i-1) % 3 == 0){
-                cnt++;
-            }else if((i+1)%3 == 0){
+            if((i-1) % 3 == 0 || (i+1)%3 == 0){
                 cnt++;
             }
         }

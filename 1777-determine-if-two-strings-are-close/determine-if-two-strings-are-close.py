c1c1bca327f36deb361f11b5bc34b6eb310b13be
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-11-23 05:37:17 +0530

    Time: 83 ms (82.15%) | Memory: 18.4 MB (20.75%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -0,0 +1,8 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1 = Counter(word1)
+        c2 = Counter(word2)
+        return c1.keys() == c2.keys() and sorted(c1.values()) == sorted(c2.values())
+
+
+        

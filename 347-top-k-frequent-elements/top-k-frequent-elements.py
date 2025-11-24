c73ced5422490c335583fa16a2314ef667e17d2b
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-11-24 06:05:43 +0530

    Time: 3 ms (90.35%) | Memory: 21.3 MB (61.06%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,9 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         t = Counter(nums)               
         ans = []
-        mc = t.most_common(k)           
-        
+        mc = t.most_common(k)
         for i in range(k):
-            ans.append(mc[i][0])         
-        
+            ans.append(mc[i][0])
         return ans

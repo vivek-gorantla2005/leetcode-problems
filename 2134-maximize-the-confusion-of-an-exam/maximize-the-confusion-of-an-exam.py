00c439d157ba9196627f144594e12e81fa0d4f8f
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-11-29 11:11:41 +0530

    Time: 359 ms (7.66%) | Memory: 17.9 MB (80.31%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxConsecutiveAnswers(self, s: str, k: int) -> int:
+        m = Counter()
+        l = 0
+        r = 0
+        maxi = 0
+        maxf = 0    
+        while r < len(s):
+            m[s[r]] += 1
+            maxf = max(maxf, m[s[r]])
+            while (r - l + 1) - maxf > k and l < len(s):
+                m[s[l]] -= 1
+                l += 1
+
+            maxi = max(maxi, r - l + 1)
+            r += 1
+
+        return maxi
+        

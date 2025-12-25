f5acfce3a5bd2b9fa934b74fcd1aa19cee11b966
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-12-26 04:14:04 +0530

    Time: 10 ms (40.77%) | Memory: 17.8 MB (94.36%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         ms = ""
         for i in s:
-            if i.isspace() or not i.isalnum():
+            if not i.isalnum():
                 continue
             if 'A' <= i <= 'Z':
                 ms+=chr(ord(i) + 32)

17ffc031dc0ef3bb27770078ed38472dc443a3aa
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2025-12-30 05:00:39 +0530

    Time: 155 ms (35.38%) | Memory: 17.9 MB (74.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,16 +3,16 @@ class Solution:
         m = Counter()
         l = 0
         r = 0
-        maxi = 0
-        maxf = 0    
+        maxi = float('-inf')
+        maxf = float('-inf')
         while r < len(s):
-            m[s[r]] += 1
-            maxf = max(maxf, m[s[r]])
-            while (r - l + 1) - maxf > k and l < len(s):
-                m[s[l]] -= 1
-                l += 1
-
-            maxi = max(maxi, r - l + 1)
-            r += 1
-
+            m[s[r]]+=1
+            maxf = max(maxf,m[s[r]])
+            while r-l+1 - maxf > k and l < len(s):
+                m[s[l]]-=1
+                l+=1
+            maxi = max(maxi,r-l+1)
+            r+=1
+    
         return maxi
+        

df0064f1888bb92825c20ec10799590e4135f80a
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-01-01 05:28:58 +0530

    Time: 104 ms (20.12%) | Memory: 22.8 MB (24.57%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,26 +1,44 @@
-class MyHashMap:
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
 
+
+class MyHashMap:
     def __init__(self):
-        self.map = {}
+        self.size = 10**4
+        self.map = [ListNode(-1, -1) for _ in range(self.size)]  # dummy heads
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.map:
-            self.map[key] = 0
-        self.map[key] = value
-    
+        idx = key % self.size
+        curr = self.map[idx]
+
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.val = value
+                return
+            curr = curr.next
+
+        curr.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        if key in self.map:
-            return self.map[key]
+        idx = key % self.size
+        curr = self.map[idx]
+
+        while curr.next:
+            if curr.next.key == key:
+                return curr.next.val
+            curr = curr.next
+
         return -1
 
     def remove(self, key: int) -> None:
-        if key in self.map:
-            del self.map[key]
+        idx = key % self.size
+        curr = self.map[idx]
 
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next

89ef49b4e5e5222c1094283f8a6c4a321b3e0942
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-01-24 06:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.30%) - LeetSync

--- 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.py
+++ 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.py
@@ -1,20 +1,16 @@
+from sortedcontainers import SortedDict
 class SummaryRanges:
-
     def __init__(self):
-        self.ranges = []
-        self.numSet =set()
+        self.treeMap = SortedDict()
 
     def addNum(self, value: int) -> None:
-        self.numSet.add(value)
+        self.treeMap[value] = True
 
     def getIntervals(self) -> List[List[int]]:
-        nums = sorted(list(self.numSet))
         res = []
-        i = 0
-        while i < len(nums):
-            start = nums[i]
-            while i+1 < len(nums) and nums[i]+1 == nums[i+1]:
-                i+=1
-            res.append([start,nums[i]])
-            i+=1
+        for n in self.treeMap:
+            if res and res[-1][1] + 1 == n:
+                res[-1][1] = n
+            else:
+                res.append([n,n])
         return res

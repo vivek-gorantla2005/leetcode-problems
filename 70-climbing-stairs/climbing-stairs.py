9cffd1c33c0ed9bf9bcc4ed4553edf97d8017233
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-01-28 05:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -13,11 +13,21 @@ class Solution:
 
             return j1+j2
         
-        dp = [0 for _ in range(n+1)]
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        # dp = [0 for _ in range(n+1)]
+        # dp[0] = 1
+        # dp[1] = 1
+        # for i in range(2,n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
         
-        return dp[n]
+        # return dp[n]
+
+        prev1 = 1
+        prev2 = 1
+        curr = 0
+        for i in range(2,n+1):
+            curr = prev1+prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+
 

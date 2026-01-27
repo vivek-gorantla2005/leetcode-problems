3e34b1de26cf792c2377c08bdcf8862d1c03071b
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-01-28 05:13:40 +0530

    Time: 135 ms (54.05%) | Memory: 20.1 MB (44.85%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -9,5 +9,13 @@ class Solution:
             add = backtrack(idx+1,total+nums[idx])
             sub = backtrack(idx+1,total-nums[idx])
             return add+sub
+
+        dp = [defaultdict(int) for _ in range(len(nums)+1)]
+        dp[0][0] = 1 #(0 elements,0 sum) -> 1 way
+
+        for i in range(len(nums)):
+            for cur_sum,count in dp[i].items():
+                dp[i+1][cur_sum + nums[i]] += count
+                dp[i+1][cur_sum-nums[i]]+=count
         
-        return backtrack(0,0)
+        return dp[len(nums)][target]

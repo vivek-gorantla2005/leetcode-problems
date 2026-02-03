5efe6080ea9dbeec937b087993f65d72a3233242
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-03 19:23:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (12.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(None)
+        dp = {}
         def dfs(idx):
             if idx >= len(nums):
                 return 0
@@ -8,10 +8,14 @@ class Solution:
             if idx == len(nums)-1:
                 return nums[idx]
             
+            if idx in dp:
+                return dp[idx]
+
             pick = nums[idx]+dfs(idx+2)
             notpick = 0 + dfs(idx+1)
 
-            return max(pick,notpick)
+            dp[idx] = max(pick,notpick)
+            return dp[idx]
         
         return dfs(0)
 

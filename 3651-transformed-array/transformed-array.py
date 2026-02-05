4c6dbafc34cc560b9b8bf4b19802fbf634478a7f
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-05 19:25:00 +0530

    Time: 65 ms (26.91%) | Memory: 19.3 MB (34.53%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -3,11 +3,8 @@ class Solution:
         n = len(nums)
         res = [0] * len(nums)
         for i in range(len(nums)):
-            if nums[i] > 0:
+            if nums[i] > 0 or nums[i] < 0: 
                 idx = (i + nums[i]) % n
-                res[i] = nums[idx]
-            elif nums[i] < 0:
-                idx = (i + nums[i])%n
                 res[i] = nums[idx]
             else:
                 res[i] = nums[i]

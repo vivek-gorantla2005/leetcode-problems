404429fe03ac1182e187bb38379d5150a6a795db
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-06 06:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (14.02%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,18 +1,36 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = {}
-        def helper(i,j):
-            if i == len(triangle)-1:
-                return triangle[i][j]
-            if (i,j) in dp:
-                return dp[(i,j)]
-            d1 = triangle[i][j] + helper(i+1,j)
-            d2 = triangle[i][j] + helper(i+1,j+1)
+    # =====================================================> memoization
+        # dp = {}
+        # def helper(i,j):
+        #     if i == len(triangle)-1:
+        #         return triangle[i][j]
 
-            dp[(i,j)] =  min(d1,d2)
-            return dp[(i,j)]
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+
+        #     d1 = triangle[i][j] + helper(i+1,j)
+        #     d2 = triangle[i][j] + helper(i+1,j+1)
+
+        #     dp[(i,j)] =  min(d1,d2)
+        #     return dp[(i,j)]
         
-        return helper(0,0)
+        # return helper(0,0)
 
+
+    # =======================================================>tabulation
+        n = len(triangle)
+        dp = [[0] * n for _ in range(n)]
+
+        dp[n-1] = triangle[-1][:]
+
+        for i in range(n-2,-1,-1):
+            for j in range(i+1):
+                dp[i][j] = triangle[i][j] + min(
+                    dp[i+1][j],
+                    dp[i+1][j+1]
+                )
+
+        return dp[0][0]
 
         

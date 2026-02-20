fc2f7331bc2d77c9ec36d8bf5f27a5a78768a06e
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-20 13:57:13 +0530

    Time: 177 ms (12.31%) | Memory: 54.8 MB (9.52%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDistance(self, text1: str, text2: str) -> int:
+        dp = {}
+        # shift indexes by 1 place instead of zero indexed array take 1 based indexing
+        def dfs(idx1,idx2):
+            if idx1 == 0 or idx2 == 0:
+                return 0
+            
+            if (idx1,idx2) in dp:
+                return dp[(idx1,idx2)]
+
+            if text1[idx1-1] == text2[idx2-1]:
+                dp[(idx1,idx2)] = 1 + dfs(idx1-1,idx2-1)
+                return dp[(idx1,idx2)]
+            
+            dp[(idx1,idx2)]  = max(dfs(idx1-1,idx2),dfs(idx1,idx2-1))
+            return dp[(idx1,idx2)]
+
+        n = len(text1)
+        m = len(text2)
+        
+        return (n-dfs(n,m)) + (m-dfs(n,m))
+        

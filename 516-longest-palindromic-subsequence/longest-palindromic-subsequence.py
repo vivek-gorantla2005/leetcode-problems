03a6e77f75c9fd0ef714f7248053f0b269bee40a
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-20 12:20:37 +0530

    Time: 1293 ms (26.35%) | Memory: 44.5 MB (36.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,23 +1,33 @@
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        dp = {}
-        # shift indexes by 1 place instead of zero indexed array take 1 based indexing
-        def dfs(str1,str2,idx1,idx2):
-            if idx1 == 0 or idx2 == 0:
-                return 0
+    def longestPalindromeSubseq(self, text1: str) -> int:
+        # dp = {}
+        # # shift indexes by 1 place instead of zero indexed array take 1 based indexing
+        # def dfs(str1,str2,idx1,idx2):
+        #     if idx1 == 0 or idx2 == 0:
+        #         return 0
             
-            if (idx1,idx2) in dp:
-                return dp[(idx1,idx2)]
+        #     if (idx1,idx2) in dp:
+        #         return dp[(idx1,idx2)]
 
-            if str1[idx1-1] == str2[idx2-1]:
-                dp[(idx1,idx2)] = 1 + dfs(str1,str2,idx1-1,idx2-1)
-                return dp[(idx1,idx2)]
+        #     if str1[idx1-1] == str2[idx2-1]:
+        #         dp[(idx1,idx2)] = 1 + dfs(str1,str2,idx1-1,idx2-1)
+        #         return dp[(idx1,idx2)]
             
-            dp[(idx1,idx2)]  = max(dfs(str1,str2,idx1-1,idx2),dfs(str1,str2,idx1,idx2-1))
-            return dp[(idx1,idx2)]
+        #     dp[(idx1,idx2)]  = max(dfs(str1,str2,idx1-1,idx2),dfs(str1,str2,idx1,idx2-1))
+        #     return dp[(idx1,idx2)]
 
-        rev = s[::-1]
-        n = len(s)
-        m = len(rev)
+        text2 = text1[::-1]
+        n = len(text1)
+        m = len(text2)
 
-        return dfs(s,rev,n,m)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j]  = max(dp[i-1][j],dp[i][j-1])
+
+
+        return dp[n][m]

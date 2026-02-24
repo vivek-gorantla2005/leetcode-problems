2771c2d18f2482edc9576f7422d597a34a1d5e90
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-25 03:02:33 +0530

    Time: 2827 ms (6.13%) | Memory: 19.4 MB (82.43%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,13 +1,13 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # dp = {}
-
+        # n = len(nums)
+        # dp = [[0] * n for _ in range(n)]
         # def dfs(idx, prevIdx):
-        #     if idx == len(nums):
+        #     if idx == n:
         #         return 0
 
-        #     if (idx, prevIdx) in dp:
-        #         return dp[(idx, prevIdx)]
+        #     if dp[idx][prevIdx + 1] != -1:
+        #         return dp[idx][prevIdx + 1]
 
         #     notPick = dfs(idx + 1, prevIdx)
 
@@ -15,26 +15,50 @@ class Solution:
         #     if prevIdx == -1 or nums[idx] > nums[prevIdx]:
         #         pick = 1 + dfs(idx + 1, idx)
 
-        #     dp[(idx, prevIdx)] = max(pick, notPick)
-        #     return dp[(idx, prevIdx)]
+        #     dp[idx][prevIdx + 1] = max(pick, notPick)
+        #     return dp[idx][prevIdx + 1]
 
         # return dfs(0, -1)
 
-        # previdx starts from -1 but we cant store -1 in arr so perform idx+1 shift
-        n = len(nums)
-        # dp[i][prev+1]
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
+# ====================================================> tabulation
+        # n = len(nums)
+        # dp = [[0] * (n+1) for _ in range(n+1)]
 
-        for i in range(n - 1, -1, -1):
-            for prev in range(i - 1, -2, -1):
-                notPick = dp[i + 1][prev + 1]
+        # for idx in range(n-1, -1, -1):
+        #     for prevIdx in range(idx-1, -2, -1):
+
+        #         notPick = dp[idx + 1][prevIdx + 1]
+
+        #         pick = 0
+        #         if prevIdx == -1 or nums[idx] > nums[prevIdx]:
+        #             pick = 1 + dp[idx + 1][idx + 1]
+
+        #         dp[idx][prevIdx + 1] = max(pick, notPick)
+
+        # return dp[0][0]
+# ===========================================================================>space optimization
+
+        n = len(nums)
+        nxt = [0] * (n+1)
+        curr = [0] * (n+1)
+
+        for idx in range(n-1, -1, -1):
+            for prevIdx in range(idx-1, -2, -1):
+                notPick = nxt[prevIdx + 1]
 
                 pick = 0
-                if prev == -1 or nums[i] > nums[prev]:
-                    pick = 1 + dp[i + 1][i + 1]
+                if prevIdx == -1 or nums[idx] > nums[prevIdx]:
+                    pick = 1 + nxt[idx + 1]
 
-                dp[i][prev + 1] = max(pick, notPick)
+                curr[prevIdx + 1] = max(pick, notPick)
+            
+            nxt = curr
 
-        return dp[0][0]
+        return nxt[0]
 
 
+
+
+        
+
+        

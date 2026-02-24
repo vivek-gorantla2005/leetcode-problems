c2e0b299760147d9e290ed06f42ee3fe1658643f
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-02-24 05:36:43 +0530

    Time: 743 ms (24.42%) | Memory: 62.5 MB (23.13%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -2,31 +2,55 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # dp = {}
+
+        # def dfs(idx, buy, cap):
+        #     if cap == 0:
+        #         return 0
+        #     if idx == n:
+        #         return 0
+
+        #     if (idx,buy,cap) in dp:
+        #         return dp[(idx,buy,cap)]
+
+        #     if buy:
+        #         profit = max(
+        #             -prices[idx] + dfs(idx+1, 0, cap),   
+        #             dfs(idx+1, 1, cap)                  
+        #         )
+        #     else:
+        #         profit = max(
+        #             prices[idx] + dfs(idx+1, 1, cap-1), 
+        #             dfs(idx+1, 0, cap)               
+        #         )
+
+        #     dp[(idx,buy,cap)] = profit
+        #     return dp[(idx,buy,cap)]
+
+        # return dfs(0, 1, 2)
+
+
         n = len(prices)
 
-        dp = {}
+        dp = [[[0] * 3 for _ in range(2)] for _ in range(n + 1)]
 
-        def dfs(idx, buy, cap):
-            if cap == 0:
-                return 0
-            if idx == n:
-                return 0
+        for idx in range(n-1,-1,-1):
+            for buy in range(0,2):
+                for cap in range(1,3):
+                    if buy:
+                        profit = max(
+                            -prices[idx] + dp[idx+1][0][cap],   
+                            dp[idx+1][1][cap]
+                        )
+                    else:
+                        profit = max(
+                            prices[idx] + dp[idx+1][1][cap-1], 
+                            dp[idx+1][0][cap]               
+                        )
 
-            if (idx,buy,cap) in dp:
-                return dp[(idx,buy,cap)]
+                    dp[idx][buy][cap] = profit
 
-            if buy:
-                profit = max(
-                    -prices[idx] + dfs(idx+1, 0, cap),   
-                    dfs(idx+1, 1, cap)                  
-                )
-            else:
-                profit = max(
-                    prices[idx] + dfs(idx+1, 1, cap-1), 
-                    dfs(idx+1, 0, cap)               
-                )
+        return dp[0][1][2]
 
-            dp[(idx,buy,cap)] = profit
-            return dp[(idx,buy,cap)]
 
-        return dfs(0, 1, 2)
+

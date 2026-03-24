2a524894c7a178f58a9c10db375b97ab9ceb402e
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-03-24 05:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (54.04%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
-        def helper(nums,idx,curr):
+        ans = []
+        def dfs(idx,curr):
             if idx == len(nums):
-                self.ans.append(curr[:])
-                return
+                ans.append(curr[:])
+                return 
             
             curr.append(nums[idx])
-            helper(nums,idx+1,curr)
+            dfs(idx+1,curr)
             curr.pop()
-            helper(nums,idx+1,curr)
-        curr = []
-        helper(nums,0,curr)
-        return self.ans
+            dfs(idx+1,curr)
 
+        dfs(0,[])
+        return ans
 
+        

2ed383f7f4c16c294318cd682f01f8e656d8ad9a
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-03-26 10:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (89.74%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -3,43 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head or not head.next:
-            return
-        
-        # 1. Find middle
         slow = head
         fast = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
         
-        # 2. Reverse second half
-        second = slow.next
-        prev = slow.next = None  
-        while second:
-            nxt = second.next
-            second.next = prev
-            prev = second
-            second = nxt
+        h2 = slow.next
+        slow.next = None
+
+        #reverse second half
+        prev = None
+        temp2 = h2
+        while temp2:
+            nxtnode = temp2.next
+            temp2.next = prev
+            prev = temp2
+            temp2 = nxtnode
+
+        t1 = head
+        t2 = prev
+        while t2:
+            first = t1.next
+            second = t2.next
+            t1.next = t2
+            t2.next = first
+
+            t1 = first
+            t2 = second
         
-        # 3. Merge both halves: head (first half) & prev (second half reversed)
-        first = head
-        second = prev
-        
-        while second:
-            # save next pointers
-            tmp1 = first.next
-            tmp2 = second.next
+        return 
             
-            # weave nodes: first → second
-            first.next = second
-            second.next = tmp1
-            
-            # move forward
-            first = tmp1
-            second = tmp2
+
+
+
         
-        return

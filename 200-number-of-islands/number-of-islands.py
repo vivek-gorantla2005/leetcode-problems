87f941b3d5dfceebc94dd5f06d14bbb4927c6aec
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-03-30 04:37:14 +0530

    Time: 262 ms (27.32%) | Memory: 31.2 MB (8.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited = [[False] * cols for _ in range(rows)]
-
-        def dfs(i, j):
-            if (
-                i < 0 or j < 0 or
-                i >= rows or j >= cols or
-                visited[i][j] or
-                grid[i][j] == "0"
-            ):
+        n = len(grid)
+        m = len(grid[0])
+        visited = set()
+        def dfs(i,j):
+            if i >= n or j >= m or i < 0 or j < 0 or (i,j) in visited or grid[i][j] == "0":
                 return
-
-            visited[i][j] = True
-
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-
+            
+            visited.add((i,j))
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i-1,j)
+            dfs(i,j-1)
+        
         ans = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1" and not visited[i][j]:
-                    dfs(i, j)
-                    ans += 1 
-
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1" and  (i,j) not in visited:
+                    ans+=1
+                    dfs(i,j)
+        
         return ans

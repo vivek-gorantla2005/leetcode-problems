d0fd45e9f4e5cbdb8a2aecda2cfaa4da99475fb1
Vivek <158069356+vivek-gorantla2005@users.noreply.github.com>
2026-03-30 05:10:20 +0530

    Time: 3 ms (87.48%) | Memory: 20.4 MB (87.09%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,33 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def canFinish(self, numCourses: int, prereq: List[List[int]]) -> bool:
         adj = [[] for _ in range(numCourses)]
         indegree = [0] * numCourses
 
+        # build graph
         for a, b in prereq:
-            adj[b].append(a)
+            adj[b].append(a)   # b → a
             indegree[a] += 1
 
         q = deque()
 
+        # push nodes with indegree 0
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
 
-        completed = 0
+        count = 0
+
+        ans = []
 
         while q:
-            course = q.popleft()
-            completed += 1
+            node = q.popleft()
+            ans.append(node)
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
 
-            for next_course in adj[course]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    q.append(next_course)
-
-        return completed == numCourses
+        return len(ans) == numCourses
